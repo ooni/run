2e5b1733f622213df61313312bdce8e812fa2d59
Arturo Filastò <arturo@filasto.net>
2019-11-18 19:01:37 +0100

    hotfix: pass in --max-http-header-size=32768 to support larger queries

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+
 app.prepare()
 .then(() => {
   const server = express()
-
   const staticDir = path.resolve(__dirname, 'static')
   server.use('/static', express.static(staticDir))
 
