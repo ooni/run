b61d0680b90efdd28b4b1e8ed8efca80858bf0ff
Maja Komel <maja.komel@gmail.com>
2023-02-07 16:51:29 +0100

    Remove unused strings (#123)

--- utils/nettest.js
+++ utils/nettest.js
@@ -3,69 +3,6 @@ import {
 } from 'ooni-components'
 import { defineMessages, useIntl } from 'react-intl'
 
-export const messages = defineMessages({
-  web_connectivity_name: {
-    id: 'Test.WebConnectivity.Name',
-    defaultMessage: 'Web Connectivity',
-  },
-  http_invalid_request_line_name: {
-    id: 'Test.HTTPInValidRequestLine.Name',
-    defaultMessage: 'HTTP Invalid Request Line'
-  },
-  http_header_field_manipulation_name: {
-    id: 'Test.HTTPHeaderFieldManipulation.Name',
-    defaultMessage: 'HTTP Header Field Manipulation'
-  },
-  ndt_name: {
-    id: 'Test.NDT.Name',
-    defaultMessage: 'NDT Speed Test'
-  },
-  dash_name: {
-    id: 'Test.Dash.Name',
-    defaultMessage: 'DASH Video Streaming'
-  },
-  web_connectivity_desc: {
-    id: 'Test.WebConnectivity.Description',
-    defaultMessage: 'Check if websites are blocked.'
-  },
-  http_invalid_request_line_desc: {
-    id: 'Test.HTTPInValidRequestLine.Description',
-    defaultMessage: 'Find middleboxes.'
-  },
-  http_header_field_manipulation_desc: {
-    id: 'Test.HTTPHeaderFieldManipulation.Description',
-    defaultMessage: 'Find middleboxes.'
-  },
-  ndt_desc: {
-    id: 'Test.NDT.Description',
-    defaultMessage: 'Measure the speed and performance of your network.'
-  },
-  dash_desc: {
-    id: 'Test.Dash.Description',
-    defaultMessage: 'Measure video streaming performance.'
-  },
-  nnText: {
-    id: 'WhatCanYouDoText.NetNeutrality',
-    defaultMessage:'Generate a link and share it with your friends and contacts around the world. Encourage them to run OONI Probe to measure the speed and performance of their networks!'
-  },
-  mbText: {
-    id: 'WhatCanYouDoText.Middleboxes',
-    defaultMessage:'Generate a link and share it with your friends and contacts around the world. Encourage them to run OONI Probe to find middleboxes in their networks!'
-  },
-  webText: {
-    id: 'WhatCanYouDoText.WebCensorship',
-    defaultMessage:'Add websites below that you would like to test for censorship. Click "Generate" to create a link based on those websites. Share that link with OONI Probe mobile app users so that they can test the websites of your choice!'
-  },
-  genericText: {
-    id: 'WhatCanYouDoText.Generic',
-    defaultMessage:'Generate a link and share it with your friends and contacts around the world.'
-  },
-  learn: {
-    id: 'Sidebar.Text.Learn',
-    defaultMessage: 'Learn how this test works here'
-  }
-})
-
 export const censorshipTests = [
   {
     key: 'web_connectivity',
@@ -120,21 +57,4 @@ export const getTestType = testName => {
   if (isIn(testName, cTestKeys)) {
     return 'Web Censorship'
   }
-}
-
-export const WhatCanYouDoText = props => {
-  const intl = useIntl()
-  if (isIn(props.test, nnTestKeys)) {
-    return <Text>{intl.formatMessage(messages.nnText)}</Text>
-  }
-
-  if (isIn(props.test, mbTestKeys)) {
-    return <Text>{intl.formatMessage(messages.mbText)}</Text>
-  }
-
-  if (isIn(props.test, cTestKeys)) {
-    return <Text>{intl.formatMessage(messages.webText)}</Text>
-  }
-
-  return <Text>{intl.formatMessage(messages.genericText)}</Text>
 }
