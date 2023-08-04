43596ac7cda4329644c72e7d844c52ee78457164
majakomel <maja.komel@gmail.com>
2023-08-04 11:11:07 +0200

    Add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,37 @@
 import { IntlProvider } from 'react-intl'
 import { useRouter } from 'next/router'
-import { useMemo } from 'react'
+import { useMemo, useEffect } from 'react'
+import NProgress from 'nprogress'
+import '../public/static/nprogress.css'
 
 import type { AppProps } from 'next/app'
 import Layout from 'components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { locale = 'en', defaultLocale } = useRouter()
+  const router = useRouter()
+  const { locale = 'en', defaultLocale } = router
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      console.log(`Loading: ${url}`)
+      NProgress.configure
+      NProgress.start()
+    }
+
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
 
   const messages = useMemo(() => {
     try {
