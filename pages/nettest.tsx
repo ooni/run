853025f6b31e11a1a1bbf25a5f991b95b0b984bd
majakomel <maja.komel@gmail.com>
2024-01-02 21:23:26 +0100

    Ude metatags component for nettests

--- pages/nettest.tsx
+++ pages/nettest.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { FormattedMessage } from 'react-intl'
 import { Container, Button, Link, Heading, Text, Box } from 'ooni-components'
 import { getIntentURI, getEncodedQuery } from 'utils/links'
@@ -7,6 +6,7 @@ import styled from 'styled-components'
 import { GetServerSideProps } from 'next'
 import { ParsedUrlQuery } from 'querystring'
 import OONIRunHeroMinimal from 'components/OONIRunHeroMinimal'
+import MetaTags from 'components/v2/MetaTags'
 
 const StyledCode = styled.code`
   font-family: courier, monospace;
@@ -114,60 +114,13 @@ const Nettest = ({
 
   return (
     <>
-      <Head>
-        <meta name="twitter:card" content="app" />
-        <meta name="twitter:site" content="@OpenObservatory" />
-
-        {/* Open Graph meta tags. Shared by Twitter and Facebook */}
-        <meta name="og:type" content="website" />
-        {universalLink && <meta name="og:url" content={universalLink} />}
-        {title && <meta name="og:title" content={title} />}
-        <meta
-          name="og:image"
-          content="https://run.ooni.io/static/images/Run-VerticalColorW400px.png"
-        />
-        {description && <meta name="og:description" content={description} />}
-
-        {/* This is Twitter specific stuff
-         * See: https://dev.twitter.com/cards/types/app */}
-        {deepLink && <meta name="twitter:app:url:iphone" content={deepLink} />}
-        {deepLink && <meta name="twitter:app:url:ipad" content={deepLink} />}
-        {universalLink && (
-          <meta name="twitter:app:url:googleplay" content={universalLink} />
-        )}
-
-        <meta
-          name="twitter:image"
-          content="https://run.ooni.io/static/images/Run-VerticalColorW400px.png"
-        />
-        <meta name="twitter:app:name:iphone" content={mobileApp.iPhoneName} />
-        <meta name="twitter:app:id:iphone" content={mobileApp.iPhoneID} />
-        <meta name="twitter:app:name:ipad" content={mobileApp.iPadName} />
-        <meta name="twitter:app:id:ipad" content={mobileApp.iPadID} />
-        <meta
-          name="twitter:app:name:googleplay"
-          content={mobileApp.googlePlayName}
-        />
-        <meta
-          name="twitter:app:id:googleplay"
-          content={mobileApp.googlePlayID}
-        />
-
-        {/* This is Facebook specific stuff
-         * See:
-         * * https://developers.facebook.com/docs/applinks/add-to-content/
-         * * https://blog.branch.io/how-to-deep-link-on-facebook/ */}
-        <meta property="al:android:package" content={mobileApp.googlePlayID} />
-        <meta
-          property="al:android:app_name"
-          content={mobileApp.googlePlayName}
-        />
-        {deepLink && <meta property="al:android:url" content={deepLink} />}
-
-        <meta property="al:ios:app_store_id" content={mobileApp.iPhoneID} />
-        <meta property="al:ios:app_name" content={mobileApp.iPhoneName} />
-        {deepLink && <meta property="al:ios:url" content={deepLink} />}
-      </Head>
+      <MetaTags 
+        title={title}
+        description={description}
+        mobileApp={mobileApp}
+        deepLink={deepLink}
+        universalLink={universalLink}
+      />
       <OONIRunHeroMinimal />
       <Container p={4}>
         <Heading pt={2} h={2}>
