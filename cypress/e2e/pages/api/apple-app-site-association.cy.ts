fe8a168d5333266af6cb6586a2e91231aa41a4e8
Maja Komel <maja.komel@gmail.com>
2024-01-04 20:32:16 +0100

    Basic e2e tests (#129)

---
+++ cypress/e2e/pages/api/apple-app-site-association.cy.ts
@@ -0,0 +1,12 @@
+import appleAppSiteAssociation from "public/static/apple-app-site-association.json";
+
+describe("apple-app-site-association", () => {
+  it("return correct json", () => {
+    cy.request({
+      method: "GET",
+      url: "/apple-app-site-association",
+    }).then((response) => {
+      expect(response.body).to.deep.include(appleAppSiteAssociation);
+    });
+  });
+});
