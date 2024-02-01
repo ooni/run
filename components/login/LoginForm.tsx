a8f61493bb3df4b3ac5534b5f47f366555811edd
majakomel <maja.komel@gmail.com>
2024-02-01 16:54:29 +0100

    Update dependencies

--- components/login/LoginForm.tsx
+++ components/login/LoginForm.tsx
@@ -26,6 +26,7 @@ export const LoginForm = ({ onLogin, redirectTo }: LoginFormProps) => {
 
   const onSubmit = useCallback(
     (data: { email_address: string }) => {
+      console.log("data", data)
       const { email_address } = data
       const registerApi = async (email_address: string) => {
         try {
