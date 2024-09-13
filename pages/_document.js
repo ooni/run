d1d1febc49a384611adc4dd8b9c3a36c0f4ff795
Maja <maja.komel@gmail.com>
2024-09-13 11:33:30 +0200

    Split umami tracking for run.ooni.io and .org (#193)

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,6 @@ export default class MyDocument extends Document {
 						defer
 						src="https://cloud.umami.is/script.js"
 						data-website-id="dbcccbba-4a4e-4a6a-a026-c2ea17e14117"
-						data-domains="run.ooni.io,run.ooni.org"
 					/>
 				</Head>
 				<body>
